c346ebdda9cc0ec4406d1abae721f97ab342e545
DanielLaRotta <166852222+DanielLaRotta@users.noreply.github.com>
2024-05-02 19:56:41 -0500

    PaginaSiguiente

--- app/src/main/java/com/example/dresswellpiloto/MainActivity.java
+++ app/src/main/java/com/example/dresswellpiloto/MainActivity.java
@@ -18,6 +18,12 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         }
+
+        public void irPrimeraPag(View view){
+
+        Intent i = new Intent(this, PrimeraPag.class);
+        startActivity(i);
+        }
         public void irRegistrar(View view){
 
             Intent i = new Intent(this, RegistrarseAc.class);
--- app/src/main/java/com/example/dresswellpiloto/PrimeraPag.java
+++ app/src/main/java/com/example/dresswellpiloto/PrimeraPag.java
@@ -1,8 +1,6 @@
 package com.example.dresswellpiloto;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,16 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class RegistrarseAc extends AppCompatActivity {
+public class PrimeraPag extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_registrarse);
-    }
-    public void irPrimeraPag (View view){
-        Intent i = new Intent(this, PrimeraPag.class);
-        startActivity(i);
+        setContentView(R.layout.activity_primera_pag);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
