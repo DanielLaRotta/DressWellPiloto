9385559730c126e437371418d809655295043979
DanielLaRotta <166852222+DanielLaRotta@users.noreply.github.com>
2024-05-27 20:00:10 -0500

    Volver
    Se agrego el boton volver a las paginas de colores para retroceder de estas a la actividad pricipal

--- app/src/main/java/com/example/dresswellpiloto/PaginaCol_FN.java
+++ app/src/main/java/com/example/dresswellpiloto/PaginaCol_FN.java
@@ -1,6 +1,8 @@
 package com.example.dresswellpiloto;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,9 @@ public class PaginaCol_FN extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void irPrimeraPag(View view){
+        Intent i = new Intent(this, PrimeraPag.class);
+        startActivity(i);
     }
 }
