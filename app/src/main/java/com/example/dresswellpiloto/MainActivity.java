cd3e56c9fb7da4721af659b5fd9d7bccc186c90b
Choma0624 <carlos.munozsegura@gmail.com>
2024-05-29 18:01:39 -0500

    Proyecto final

--- app/src/main/java/com/example/dresswellpiloto/MainActivity.java
+++ app/src/main/java/com/example/dresswellpiloto/MainActivity.java
@@ -10,23 +10,81 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dresswellpiloto.R;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
 public class MainActivity extends AppCompatActivity {
+    EditText usuario, contrasena;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        }
 
-        public void irPrimeraPag(View view){
+        usuario = findViewById(R.id.namelogin);
+        contrasena = findViewById(R.id.contralogin);
 
+        SharedPreferences credenciales = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        usuario.setText(credenciales.getString("email", ""));
+        contrasena.setText(credenciales.getString("contra", ""));
+        login = findViewById(R.id.btnlogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciosesion(
+                        usuario.getText().toString(),
+                        contrasena.getText().toString()
+                );
+            }
+        });
+    }
+
+    public void irPrimeraPag(View view) {
         Intent i = new Intent(this, PrimeraPag.class);
         startActivity(i);
-        }
-        public void irRegistrar(View view){
+    }
 
-            Intent i = new Intent(this, RegistrarseAc.class);
-            startActivity(i);
-        }
+    public void irRegistrar(View view) {
+        Intent i = new Intent(this, RegistrarseAc.class);
+        startActivity(i);
+    }
+
+    private void iniciosesion(String correo, String contra) {
+        db.collection("Usuario").document(correo).get().addOnSuccessListener(tarea -> {
+            String nombre1 = (String) tarea.get("nombre");
+            String apellido1 = (String) tarea.get("apellido");
+            String contra1 = (String) tarea.get("contra");
+
+
+            if (contra.equals(contra1)) {
+                irPrimeraPag(null);
+                Toast.makeText(this, "Aprobado", Toast.LENGTH_SHORT).show();
+
+            } else {
+                Toast.makeText(this, "Usuario y/o contraseña incorrecta", Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(error -> {
+            Toast.makeText(this, "Usuario y/o contraseña incorrecta", Toast.LENGTH_LONG).show();
+        });
+    }
 }
+
--- app/src/main/java/com/example/dresswellpiloto/RegistrarseAc.java
+++ app/src/main/java/com/example/dresswellpiloto/RegistrarseAc.java
@@ -1,25 +1,81 @@
 package com.example.dresswellpiloto;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.dresswellpiloto.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class RegistrarseAc extends AppCompatActivity {
 
+    EditText nombre, apellido, correo, contra;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private Button registro;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registrarse);
+
+        correo = findViewById(R.id.correo);
+        nombre = findViewById(R.id.nombre);
+        apellido = findViewById(R.id.apellido);
+        contra = findViewById(R.id.contra);
+        registro = findViewById(R.id.button);
+
+        SharedPreferences credenciales = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        correo.setText(credenciales.getString("email", ""));
+        nombre.setText(credenciales.getString("nombre", ""));
+        apellido.setText(credenciales.getString("apellido", ""));
+        contra.setText(credenciales.getString("contra", ""));
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciosesion(correo.getText().toString(), nombre.getText().toString(), apellido.getText().toString(), contra.getText().toString());
+            }
+        });
     }
-    public void irPrimeraPag (View view){
-        Intent i = new Intent(this, PrimeraPag.class);
-        startActivity(i);
+
+    private void iniciosesion(String correo, String nombre, String apellido, String contra) {
+        registro.setEnabled(false);
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("correo", correo);
+        map.put("nombre", nombre);
+        map.put("apellido", apellido);
+        map.put("contra", contra);
+
+        db.collection("Usuario").document(correo).set(map)
+                .addOnCompleteListener(t -> {
+                    registro.setEnabled(true);
+                })
+                .addOnSuccessListener(t -> {
+                    registro.setEnabled(true);
+                    startActivity(new Intent(RegistrarseAc.this, PrimeraPag.class));
+                    Toast.makeText(this, "Aprobado", Toast.LENGTH_SHORT).show();
+                }).addOnFailureListener(t -> {
+                    registro.setEnabled(true);
+                    Toast.makeText(this, "Usuario y/o contraseña incorrecta", Toast.LENGTH_LONG).show();
+                });
     }
 }
+
+
